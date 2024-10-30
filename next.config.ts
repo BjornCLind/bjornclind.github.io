6be16e442fb9bfbd84ad9971bfde2954c821bb31
Bjorn Christian Lindqvist <bclindqvist@gmail.com>
2024-10-29 18:41:01 -1000

    remove theme provider

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  distDir: 'docs',
   /* You can add more config options here as needed */
 };
 
