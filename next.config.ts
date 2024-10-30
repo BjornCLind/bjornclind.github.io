a538c58b8e485d0468e027e1292f0660787e009d
Bjorn Christian Lindqvist <bclindqvist@gmail.com>
2024-10-29 14:18:29 -1000

    static deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'docs',
+  /* You can add more config options here as needed */
 };
 
 export default nextConfig;
