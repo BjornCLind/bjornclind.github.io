dc197ffd923c8564dbd08dc22d6015fce9ee3073
Bjorn C Lindqvist <bclindqvist@gmail.com>
2024-10-31 12:37:34 -1000

    updating data

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "GraphQL", "Typescript"];
+  const rightLists = ["NextJS", "MongoDB", "REST"];
 
   const [copied, setCopied] = useState(false);
 
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "bjornlndq@proton.me";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
